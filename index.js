dbd1be269e103a43853f4d03c3d29e6f59c4cf0a
vpnsieucap <114736379+Okok12345@users.noreply.github.com>
2026-05-03 16:42:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 const { spawn } = require("child_process");
 const log = require("./utils/log");
 require('./includes/chess/covua');
